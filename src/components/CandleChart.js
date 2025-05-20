4fdda43ccf72bcc80798b04e4683fa3cfbfe3163
NotSomeStudent <363449737@gotvdsb.ca>
2025-05-20 10:04:45 -0400

    Update CandleChart.js

--- src/components/CandleChart.js
+++ src/components/CandleChart.js
@@ -1,1 +1,43 @@
 
+import React from 'react';
+import { Chart, registerables } from 'chart.js';
+import {
+  FinancialController,
+  CandlestickElement
+} from 'chartjs-chart-financial';
+import { Chart as ReactChart } from 'react-chartjs-2';
+
+Chart.register(
+  ...registerables,
+  FinancialController,
+  CandlestickElement
+);
+
+export default function CandleChart({ data }) {
+  const cfg = {
+    data: {
+      datasets: [
+        {
+          label: 'Price',
+          data
+        }
+      ]
+    },
+    options: {
+      scales: {
+        x: { type: 'time', time: { unit: 'hour' } },
+        y: { position: 'right' }
+      },
+      plugins: {
+        zoom: {
+          zoom: {
+            wheel: { enabled: true },
+            pinch: { enabled: true },
+            mode: 'x'
+          }
+        }
+      }
+    }
+  };
+  return <ReactChart type="candlestick" {...cfg} />;
+}
