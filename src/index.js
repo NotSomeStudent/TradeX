efc940dd7e9ab82a94194625aff93933ed4bd77f
NotSomeStudent <363449737@gotvdsb.ca>
2025-05-20 10:00:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { PortfolioProvider } from './context/PortfolioContext';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <AuthProvider>
+    <PortfolioProvider>
+      <App />
+    </PortfolioProvider>
+  </AuthProvider>
+);
