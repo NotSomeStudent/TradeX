7a97f412b617ae6e579033cb91545955cdd4c569
NotSomeStudent <363449737@gotvdsb.ca>
2025-06-05 13:39:25 -0400

    Update formatDate.js

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,11 @@
 export function formatTimestamp(ts) {
   const d = new Date(ts);
   return d.toLocaleString(undefined, {
-    year: 'numeric', month: 'short', day: 'numeric',
-    hour: '2-digit', minute: '2-digit', second: '2-digit'
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
   });
 }
