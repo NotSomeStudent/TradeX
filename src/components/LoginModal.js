0eca6fb498e0f73cd50e4c2bfa316d38e132a22e
NotSomeStudent <363449737@gotvdsb.ca>
2025-06-05 13:40:36 -0400

    Update LoginModal.js

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
 export default function LoginModal() {
+  // We already handle passphrase in AuthContext,
+  // so no UI needed here. Return null.
   const { cryptoKey } = useContext(AuthContext);
-  // once cryptoKey is set, modal never shows
   return null;
 }
